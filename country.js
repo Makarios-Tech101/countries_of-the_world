df9ebb8e17e96dc08f32a54b1ab01b0cc06a927d
Makarios-Tech101 <ogodogublessing1@gmail.com>
2024-02-25 13:17:15 +0100

    "filter by region updated"

--- country.js
+++ country.js
@@ -1,7 +1,63 @@
 const countryName = (new URLSearchParams(window.location.search).get('name'))
+const flagImage = document.querySelector('.countryDetails img')
+const countryTitle = document.querySelector('.countryDetails h2')
+const nativeName = document.querySelector('.nativeName')
+const population = document.querySelector('.population')
+const region = document.querySelector('.region')
+const subRegion = document.querySelector('.subRegion')
+const capital = document.querySelector('.capital')
+const topLevelDomain = document.querySelector('.topLevelDomain')
+const currencies = document.querySelector('.currencies')
+const languages = document.querySelector('.languages')
+const borderCountries = document.querySelector('.borderCountries')
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
  .then((response) => response.json())
- .then((data) => {
-    console.log(data)
+ .then(([country]) => {
+    // console.log(country)
+
+    flagImage.src = country.flags.svg;
+    countryTitle.innerText = country.name.common;
+
+    if (country.name.nativeName) {
+      nativeName.innerText = Object.values(country.name.nativeName)[0].common
+    } else {
+      nativeName.innerText = country.name.common;
+    }
+
+    population.innerText = country.population;
+    region.innerText = country.region;
+    subRegion.innerText = country.subregion;
+    capital.innerText = country.capital?.[0];
+    topLevelDomain.innerText = country.tld;
+
+    if (country.currencies) {
+      currencies.innerText = Object.values(country.currencies).map((currency) => currency.name).join(',')
+    }else {
+      currencies.innerText = country.currencies.name
+    }
+
+    if (country.languages) {
+      languages.innerText = Object.values(country.languages).map((language) => language).join(', ')
+    }
+    
+    if(country.borders) {
+      country.borders.forEach((border) => {
+        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+        .then((res) => res.json())
+        .then(([borderCountry]) => {
+          console.log(borderCountry)
+
+
+        const borderCountryTag = document.createElement('a')
+        borderCountryTag.innerText = borderCountry.name.common
+        console.log(borderCountryTag) 
+
+        borderCountries.append(borderCountryTag)
+        })
+
+      })
+    }
+
  })
